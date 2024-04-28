4cca786ce4fdf77068e3710b82f5851a72f661f0
blue-script <alexm92@inbox.ru>
2024-04-28 12:44:42 +0500

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { StrictMode } from 'react'
 import { App } from '@/App'
 import { createRoot } from 'react-dom/client'
 
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import './styles/index.scss'
+
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
