d5d6f1d8c8189968f2ba2b431e245be9ced7ddee
Nikki Odd <archiektor@gmail.com>
2024-05-01 21:24:17 +0200

    feat: add disabled effect

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -2,70 +2,57 @@ import { useState } from 'react'
 
 import { ArrowIosDownOutline, ArrowIosUp } from '@/assets/icons'
 import { Typography } from '@/components/ui/typography'
-import { ChevronDownIcon } from '@radix-ui/react-icons'
 import * as RadixSelect from '@radix-ui/react-select'
 import { SelectItem } from '@radix-ui/react-select'
+import clsx from 'clsx'
 
 import s from './select.module.scss'
 
 type SelectProps = {
+  disabled?: boolean
   placeholder?: string
 }
 
-export const Select = ({ placeholder = 'Select-box' }: SelectProps) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false)
+const availableOptions = ['apple', 'banana', 'blueberry', 'grapes', 'pineapple']
+
+export const Select = ({ disabled = false, placeholder = 'Select-box' }: SelectProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const iconColor = disabled ? '#4c4c4c' : '#fff'
 
   return (
-    <RadixSelect.Root onOpenChange={setIsOpen}>
+    <RadixSelect.Root disabled={disabled} onOpenChange={setIsOpen}>
       <div className={s.SelectWrapper}>
-        <Typography className={s.SelectTypo} color={'light'} option={'body2'}>
+        <Typography
+          className={clsx(s.SelectTypo, { [s.disabled]: disabled })}
+          color={'light'}
+          option={'body2'}
+        >
           {placeholder}
         </Typography>
-        <RadixSelect.Trigger aria-label={'Options'} className={s.SelectTrigger}>
+        <RadixSelect.Trigger
+          aria-label={'Options'}
+          className={clsx(s.SelectTrigger, { [s.disabled]: disabled })}
+        >
           <RadixSelect.Value placeholder={placeholder} />
           <RadixSelect.Icon asChild className={s.SelectIcon}>
-            {isOpen ? <ArrowIosUp color={'#fff'} /> : <ArrowIosDownOutline color={'#fff'} />}
+            {isOpen ? <ArrowIosUp color={iconColor} /> : <ArrowIosDownOutline color={iconColor} />}
           </RadixSelect.Icon>
         </RadixSelect.Trigger>
         <RadixSelect.Portal>
           <RadixSelect.Content className={s.SelectContent}>
-            <RadixSelect.Viewport className={'SelectViewport'}>
-              <RadixSelect.Group style={{ width: '210px' }}>
-                <SelectItem className={s.SelectItem} value={'apple'}>
-                  Apple
-                </SelectItem>
-                <SelectItem className={s.SelectItem} value={'banana'}>
-                  Banana
-                </SelectItem>
-                <SelectItem className={s.SelectItem} value={'blueberry'}>
-                  Blueberry
-                </SelectItem>
-                <SelectItem className={s.SelectItem} value={'grapes'}>
-                  Grapes
-                </SelectItem>
-                <SelectItem className={s.SelectItem} value={'pineapple'}>
-                  Pineapple
-                </SelectItem>
+            <RadixSelect.Viewport>
+              <RadixSelect.Group className={s.SelectGroup}>
+                {availableOptions.map(option => (
+                  <SelectItem className={s.SelectItem} key={option} value={option}>
+                    {option.charAt(0).toUpperCase() + option.slice(1)}
+                  </SelectItem>
+                ))}
               </RadixSelect.Group>
-              <RadixSelect.Separator className={'SelectSeparator'} />
             </RadixSelect.Viewport>
-            <RadixSelect.ScrollDownButton className={'SelectScrollButton'}>
-              <ChevronDownIcon />
-            </RadixSelect.ScrollDownButton>
           </RadixSelect.Content>
         </RadixSelect.Portal>
       </div>
     </RadixSelect.Root>
   )
 }
-
-// const SelectItem = React.forwardRef(({ children, className, ...props }, forwardedRef) => {
-//   return (
-//     <Select.Item className={classnames('SelectItem', className)} {...props} ref={forwardedRef}>
-//       <Select.ItemText>{children}</Select.ItemText>
-//       <Select.ItemIndicator className={'SelectItemIndicator'}>
-//         <CheckIcon />
-//       </Select.ItemIndicator>
-//     </Select.Item>
-//   )
-// })
