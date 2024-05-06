b02203fcced6ee2d6dfa9037c9f453f7092604d8
Nikki Odd <archiektor@gmail.com>
2024-05-06 12:59:33 +0200

    fix: repair checkbox

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,6 +1,3 @@
-import { ElementRef, forwardRef } from 'react'
-
-import { Typography } from '@/components/ui/typography'
 import * as RadixCheckbox from '@radix-ui/react-checkbox'
 import { CheckIcon } from '@radix-ui/react-icons'
 import clsx from 'clsx'
@@ -8,40 +5,33 @@ import clsx from 'clsx'
 import s from './checkbox.module.scss'
 
 type CheckboxProps = {
+  checked?: boolean
   className?: string
   disabled?: boolean
   id?: string
-  isChecked?: boolean
   label?: string
-  onChange?: (isChecked: boolean) => void
+  onChange?: (checked: boolean) => void
 }
 
-export const Checkbox = forwardRef<ElementRef<'form'>, CheckboxProps>(
-  ({ disabled = false, isChecked = false, label, onChange }, ref) => {
-    return (
-      <form className={s.wrapper} ref={ref}>
-        <RadixCheckbox.Root
-          checked={isChecked}
-          className={clsx(s.CheckboxRoot, { [s.selected]: isChecked }, { [s.disabled]: disabled })}
-          disabled={disabled}
-          onCheckedChange={onChange}
-        >
-          <RadixCheckbox.Indicator className={s.CheckboxIndicator}>
-            <CheckIcon className={s.CheckIcon} />
-          </RadixCheckbox.Indicator>
-        </RadixCheckbox.Root>
-        {label && (
-          <Typography
-            as={'p'}
-            className={s.marginLeft}
-            color={'light'}
-            disabled={disabled}
-            option={'body2'}
-          >
-            {label}
-          </Typography>
-        )}
-      </form>
-    )
-  }
+export const Checkbox = ({ checked, className, disabled, id, label, onChange }: CheckboxProps) => (
+  <form>
+    <div style={{ alignItems: 'center', display: 'flex' }}>
+      <RadixCheckbox.Root
+        checked={checked}
+        className={clsx(s.CheckboxRoot, { [s.disabled]: disabled }, className)}
+        defaultChecked={false}
+        id={id}
+        onCheckedChange={onChange}
+      >
+        <RadixCheckbox.Indicator className={clsx(s.CheckboxIndicator, { [s.checked]: checked })}>
+          <CheckIcon />
+        </RadixCheckbox.Indicator>
+      </RadixCheckbox.Root>
+      {label && (
+        <label className={`${s.Label}`} htmlFor={id}>
+          {label}
+        </label>
+      )}
+    </div>
+  </form>
 )
