db9055637384a7f90d8d48849305c45e2e5d9b7d
Nikki Odd <archiektor@gmail.com>
2024-05-09 07:29:36 +0200

    feat: ended with css

--- src/components/auth/forgot-form/forgot-form.tsx
+++ src/components/auth/forgot-form/forgot-form.tsx
@@ -1,7 +1,7 @@
 import { CSSProperties } from 'react'
 import { useForm } from 'react-hook-form'
 
-import { Typography } from '@/components'
+import { Button, Typography } from '@/components'
 import { FormTextField } from '@/components/ui/form/form-textfield'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { clsx } from 'clsx'
@@ -15,8 +15,23 @@ type Props = {
   style?: CSSProperties
 }
 
+const emailCustomValidation = z
+  .string()
+  .email({ message: 'Invalid email format' })
+  .refine(
+    value => {
+      const parts = value.split('@')
+
+      return parts[0].length > 1 && parts[1].length > 3
+    },
+    {
+      message:
+        "Email must have more than 1 character before '@' and more than 3 characters after '@'",
+    }
+  )
+
 const loginSchema = z.object({
-  email: z.string().trim().email({ message: 'Invalid email format' }),
+  email: emailCustomValidation,
 })
 
 export type FormValuesFromForgot = z.infer<typeof loginSchema>
@@ -36,6 +51,20 @@ export const ForgotForm = ({ className, onSubmit, style }: Props) => {
           {'Forgot your password?'}
         </Typography>
         <FormTextField className={s.emailField} control={control} label={'Email'} name={'email'} />
+        <Typography className={s.emailText} color={'light'} option={'body2'}>
+          {'Enter your email address and we will send you further instructions'}
+        </Typography>
+        <Button className={s.sendBtn} fullWidth>
+          <Typography color={'light'} option={'subtitle2'}>
+            Send Instructions
+          </Typography>
+        </Button>
+        <Typography className={s.rememberText} color={'light'} option={'body2'}>
+          {'Did you remember your password?'}
+        </Typography>
+        <Button as={'a'} className={s.linkBtn} variant={'link'}>
+          Try logging in
+        </Button>
       </form>
     </>
   )
