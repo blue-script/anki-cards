77a9b56a5b69ecb488aa5a087c285efda692e4b0
Nikki Odd <archiektor@gmail.com>
2024-05-09 12:02:32 +0200

    fix: storyfix

--- src/components/auth/checkEmail/check-email-form.tsx
+++ src/components/auth/checkEmail/check-email-form.tsx
@@ -7,12 +7,13 @@ import s from './check-email.module.scss'
 
 type Props = {
   className?: string
+  onSubmit?: () => void
   style?: CSSProperties
 }
 
-export const CheckEmailForm = ({ className, style }: Props) => {
+export const CheckEmailForm = ({ className, onSubmit, style }: Props) => {
   return (
-    <form className={clsx(s.form, className)} style={style}>
+    <form className={clsx(s.form, className)} onSubmit={onSubmit} style={style}>
       <Typography as={'h1'} className={s.headerText} color={'light'} option={'h1'}>
         {'Check Email'}
       </Typography>
@@ -28,7 +29,8 @@ export const CheckEmailForm = ({ className, style }: Props) => {
         as={'a'}
         className={s.linkBtn}
         fullWidth
-        href={'http://localhost:5173/signIn'}
+        //href={'http://localhost:5173/signIn'}
+        onClick={onSubmit}
         variant={'primary'}
       >
         Back to Sign In
--- src/components/auth/checkEmail/check-email.stories.tsx
+++ src/components/auth/checkEmail/check-email.stories.tsx
@@ -1,4 +1,5 @@
 import { CheckEmailForm } from '@/components/auth/checkEmail/check-email-form'
+import { action } from '@storybook/addon-actions'
 import { Meta, StoryObj } from '@storybook/react'
 
 const meta: Meta<typeof CheckEmailForm> = {
@@ -7,8 +8,10 @@ const meta: Meta<typeof CheckEmailForm> = {
   title: 'Auth/CheckEmailForm',
 }
 
+const action1 = action('Forward to Sign In form')
+
 export default meta
 
 export const CheckEmailForm1: StoryObj<typeof meta> = {
-  render: () => <CheckEmailForm />,
+  render: () => <CheckEmailForm onSubmit={action1} />,
 }
