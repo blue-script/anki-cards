3b2975335768e669f8f7361d7373455ab3724c41
blue-script <alexm92@inbox.ru>
2024-05-09 19:57:06 +0500

    fix: edit App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,3 @@
-import { useState } from 'react'
-
-import { Slider } from '@/components'
-
 export function App() {
-  const [values, setValues] = useState([1, 100])
-  const valueChangeHandler = (value: number[]) => {
-    setValues(value)
-  }
-
-  return (
-    <div
-      style={{ alignItems: 'center', display: 'flex', height: '100vh', justifyContent: 'center' }}
-    >
-      <Slider
-        label={'Number of cards'}
-        max={100}
-        min={0}
-        onValueChange={valueChangeHandler}
-        value={values}
-      />
-    </div>
-  )
+  return <div>Hello</div>
 }
