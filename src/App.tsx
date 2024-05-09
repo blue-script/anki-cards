b4bb037262be3fc3c897cfb8cf38726c27ed1708
blue-script <alexm92@inbox.ru>
2024-05-10 00:53:56 +0500

    fix: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,3 @@
-import { Select } from '@/components'
-
 export function App() {
-  // return <div>Hello</div>
-  return (
-    <div style={{ padding: '10px' }}>
-      <>
-        <Select
-          defaultValue={'Apple'}
-          options={['Apple', 'Banana', 'Blueberry', 'Grapes', 'Pineapple']}
-        />
-        sdfdsf
-      </>
-    </div>
-  )
+  return <div>Hello</div>
 }
