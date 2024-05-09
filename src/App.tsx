0ddb2d28c6c690ea2afc7f2e87e158dac73f4f8e
blue-script <alexm92@inbox.ru>
2024-05-09 19:50:07 +0500

    feat: add slider style for mobile version

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
+import { useState } from 'react'
+
+import { Slider } from '@/components'
+
 export function App() {
-  return <div>Hello</div>
+  const [values, setValues] = useState([1, 100])
+  const valueChangeHandler = (value: number[]) => {
+    setValues(value)
+  }
+
+  return (
+    <div
+      style={{ alignItems: 'center', display: 'flex', height: '100vh', justifyContent: 'center' }}
+    >
+      <Slider
+        label={'Number of cards'}
+        max={100}
+        min={0}
+        onValueChange={valueChangeHandler}
+        value={values}
+      />
+    </div>
+  )
 }
