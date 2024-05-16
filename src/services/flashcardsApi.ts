87edd561d48fe5b547a9f735b900d957fcd1e78c
blue-script <alexm92@inbox.ru>
2024-05-16 16:40:11 +0500

    feat: add selectFromResult to cardsPage, setupListeners to store and GradeType to cards.types

--- src/services/flashcardsApi.ts
+++ src/services/flashcardsApi.ts
@@ -1,15 +1,20 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react'
 
 export const flashcardsApi = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api.flashcards.andrii.es',
-    credentials: 'include',
-    prepareHeaders: headers => {
-      headers.append('x-auth-skip', 'true')
-    },
-  }),
+  baseQuery: retry(
+    fetchBaseQuery({
+      baseUrl: 'https://api.flashcards.andrii.es',
+      credentials: 'include',
+      prepareHeaders: headers => {
+        headers.append('x-auth-skip', 'true')
+      },
+    })
+    // { maxRetries: 3 }
+  ),
   endpoints: () => ({}),
   keepUnusedDataFor: 1,
   reducerPath: 'flashcardsApi',
+  // refetchOnFocus: true,
+  // refetchOnReconnect: true,
   tagTypes: ['Decks', 'Cards'],
 })
