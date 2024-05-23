efe09334ac1c5e1b960b81e64158bf10c18b2eaa
Nikki Odd <archiektor@gmail.com>
2024-05-23 13:11:39 +0200

    feat: finish decksLists

--- src/pages/decks/decksList/decksList.tsx
+++ src/pages/decks/decksList/decksList.tsx
@@ -1,31 +1,18 @@
-import { ChangeEvent, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { useSearchParams } from 'react-router-dom'
 
-import { ImageOutline, TrashOutline } from '@/assets/icons'
+import { TrashOutline } from '@/assets/icons'
 import { DecksTable } from '@/entities/decks'
+import { AddNewDeckModal } from '@/pages/decks/decksList/addNewDeckModal'
 import {
-  useCreateDeckMutation,
   useDeleteDeckMutation,
   useGetDecksQuery,
   useUpdateDeckMutation,
 } from '@/services/decks/decks.service'
-import {
-  Button,
-  FormCheckbox,
-  FormTextField,
-  Modal,
-  Page,
-  Pagination,
-  Slider,
-  TabSwitcher,
-  Typography,
-} from '@/shared'
+import { Button, FormTextField, Page, Pagination, Slider, TabSwitcher, Typography } from '@/shared'
 import { useDebounce } from '@/shared/hooks/useDebounce'
-import { CountButton } from '@/shared/ui/modal/footer/footer'
-import { zodResolver } from '@hookform/resolvers/zod'
 import { clsx } from 'clsx'
-import { z } from 'zod'
 
 import s from './decksList.module.scss'
 
@@ -41,6 +28,7 @@ export const DecksList = () => {
   const [maxCardsCount, setMaxCardsCount] = useState(10)
   const [currentPage, setCurrentPage] = useState(1)
   const debounceText = useDebounce<string>(search, 500)
+
   const { data: decks, refetch } = useGetDecksQuery({
     currentPage,
     itemsPerPage,
@@ -49,70 +37,44 @@ export const DecksList = () => {
     maxCardsCount,
     minCardsCount,
   })
+
   const [updateDeck] = useUpdateDeckMutation()
   const [deleteDeck] = useDeleteDeckMutation()
-
   const [open, setOpen] = useState<boolean>(false)
-
-  const { control, handleSubmit, reset } = useForm<{ name: string }>({
-    defaultValues: {
-      name: '',
-    },
+  const { control, reset } = useForm<{ name: string }>({
+    defaultValues: { name: '' },
   })
 
   useEffect(() => {
-    // Refetch data when tab, itemsPerPage, currentPage changes
     refetch()
   }, [tabValue, itemsPerPage, currentPage, refetch])
 
-  const tabValueHandler = (value: string) => {
-    setTabValue(value as tabValueT)
-  }
+  const tabValueHandler = (value: string) => setTabValue(value as tabValueT)
 
   const handleSearchChange = (value: string) => {
-    if (value.length) {
-      searchParams.set('search', value)
-    } else {
-      searchParams.delete('search')
-    }
+    searchParams.set('search', value || '')
     setSearchParams(searchParams)
   }
 
-  const pageChangeHandler = (pageNumber: number) => {
-    setCurrentPage(pageNumber)
-    refetch()
-  }
+  const pageChangeHandler = (pageNumber: number) => setCurrentPage(pageNumber)
 
-  const handleItemsPerPage = (numOfItems: string) => {
-    setItemsPerPage(+numOfItems)
-  }
+  const handleItemsPerPage = (numOfItems: string) => setItemsPerPage(+numOfItems)
 
-  const onSubmit = handleSubmit(data => {
-    console.log('Form data:', data) // Added console.log to see the form data
-  })
-
-  const sliderHandler = (data: any) => {
-    //console.log(data)
+  const sliderHandler = (data: [number, number]) => {
     setMinCardsCount(data[0])
     setMaxCardsCount(data[1])
   }
 
   return (
     <Page className={s.wrapper} mt={'10px'}>
-      {open && <AddNewDeckModal open={open} setOpen={setOpen!} title={'Add new deck'} />}
+      {open && <AddNewDeckModal open={open} setOpen={setOpen} title={'Add new deck'} />}
       <div className={s.rowContainer}>
         <Typography as={'h1'} option={'h1'}>
           Decks List
         </Typography>
-        <Button
-          onClick={() => {
-            setOpen(!open)
-          }}
-        >
-          Add new deck
-        </Button>
+        <Button onClick={() => setOpen(!open)}>Add new deck</Button>
       </div>
-      <form onSubmit={onSubmit} style={{ width: '100%' }}>
+      <form style={{ width: '100%' }}>
         <div className={clsx(s.rowContainer, s.rowHeight)}>
           <FormTextField
             control={control}
@@ -133,11 +95,9 @@ export const DecksList = () => {
             label={'Number of cards'}
             max={10}
             min={0}
-            onValueChange={sliderData => {
-              sliderHandler(sliderData)
-            }}
+            onValueChange={sliderHandler}
             value={[minCardsCount, maxCardsCount]}
-          ></Slider>
+          />
           <Button
             icon
             onClick={() => {
@@ -147,8 +107,7 @@ export const DecksList = () => {
             }}
             variant={'secondary'}
           >
-            <TrashOutline />
-            Clear filter
+            <TrashOutline /> Clear filter
           </Button>
         </div>
         <div className={s.rowContainer}>
@@ -159,16 +118,14 @@ export const DecksList = () => {
             onDeleteClick={id => {
               deleteDeck({ id })
             }}
-            onEditClick={id => {
-              updateDeck({ id, name: 'hotPeppers new deck' })
-            }}
+            onEditClick={id => updateDeck({ id, name: 'hotPeppers new deck' })}
           />
         </div>
       </form>
       <div className={s.rowContainer}>
         <Pagination
           currentPage={decks?.pagination.currentPage || 1}
-          onPageChange={pageNumber => pageChangeHandler(pageNumber)}
+          onPageChange={pageChangeHandler}
           pageSize={decks?.pagination.itemsPerPage || 10}
           setPageSize={numOfItems => handleItemsPerPage(numOfItems.toString())}
           style={{ marginTop: '15px' }}
@@ -176,105 +133,5 @@ export const DecksList = () => {
         />
       </div>
     </Page>
-  )
-}
-type AddNewDeckModalProps = {
-  open: boolean
-  setOpen: (open: boolean) => void
-  title?: string
-}
-
-const newDeckSchema = z.object({
-  cover: z
-    .string()
-    .optional()
-    .refine(val => !val || /\.(jpg|jpeg|png|gif|bmp)$/.test(val), {
-      message: 'Invalid image path. Must be a valid image format',
-    }),
-  isPrivate: z.boolean(),
-  name: z
-    .string()
-    .min(3, { message: 'Deck Name must be at least 3 characters long' })
-    .max(30, { message: 'Deck Name must not exceed 30 characters' }),
-})
-
-export type FormValuesFromAddDeck = z.infer<typeof newDeckSchema>
-
-export const AddNewDeckModal = ({ open, setOpen, title }: AddNewDeckModalProps) => {
-  const [createDeck] = useCreateDeckMutation()
-  const { control, handleSubmit, reset, setValue, watch } = useForm<FormValuesFromAddDeck>({
-    defaultValues: {
-      cover: '',
-      isPrivate: true,
-      name: '',
-    },
-    resolver: zodResolver(newDeckSchema),
-  })
-
-  const imagePath = watch('cover')
-
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0]
-
-    if (file) {
-      setValue('cover', file.name) // Update form state with the file name
-    }
-  }
-
-  const handleUploadClick = () => {
-    const fileInput = document.getElementById('fileInput') as HTMLInputElement
-
-    if (fileInput) {
-      fileInput.click()
-    }
-  }
-
-  const addFormClickHandler = (data: FormValuesFromAddDeck) => {
-    createDeck(data)
-    reset()
-  }
-
-  const openChangeHandler = () => {
-    setOpen(!open)
-  }
-
-  return (
-    <form>
-      <Modal onOpenChange={openChangeHandler} open={open} title={title}>
-        <FormTextField
-          control={control}
-          fullWidth
-          label={'Deck Name'}
-          name={'name'}
-          placeholder={'DeckName'}
-        />
-        <input
-          accept={'image/*'}
-          id={'fileInput'}
-          onChange={handleFileChange}
-          style={{ display: 'none' }}
-          type={'file'}
-        />
-        <Button className={s.uploadBtn} fullWidth onClick={handleUploadClick} variant={'secondary'}>
-          <ImageOutline />
-          {imagePath || 'Upload Image'}
-        </Button>
-        <FormCheckbox
-          className={s.privateBox}
-          control={control}
-          label={'Private Deck'}
-          name={'isPrivate'}
-        />
-        <Modal.Footer
-          countButton={CountButton.Two}
-          firstButtonHandler={handleSubmit(addFormClickHandler)}
-          firstButtonName={'Add New Deck'}
-          secondButtonHandler={() => {
-            openChangeHandler()
-          }}
-          secondButtonName={'Cancel'}
-        />
-      </Modal>
-    </form>
   )
 }
