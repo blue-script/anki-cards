92c639e1cbf5d6fa0addc423f037601809a835e1
blue-script <alexm92@inbox.ru>
2024-05-24 17:02:47 +0500

    feat: add card modal

--- src/entities/deck/addCardModal/addCardModal.tsx
+++ src/entities/deck/addCardModal/addCardModal.tsx
@@ -1,4 +1,10 @@
-import { Modal } from '@/shared'
+import { ChangeEvent, useRef } from 'react'
+
+import { Edit2 } from '@/assets/icons'
+import { useCreateCardMutation } from '@/services/cards/cards.service'
+import { Button, Modal, TextField } from '@/shared'
+
+import s from '@/features/profile/profileAvatar/profileAvatar.module.scss'
 
 type Props = {
   onOpenChange: () => void
@@ -6,9 +12,60 @@ type Props = {
 }
 
 export const AddCardModal = ({ onOpenChange, open }: Props) => {
+  const [createCard] = useCreateCardMutation()
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const selectFileHandler = () => {
+    inputRef.current?.click()
+  }
+
+  const uploadHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+
+    if (file) {
+      if (!file.type.match('image.*')) {
+        alert('Please select a valid image file.')
+
+        return
+      }
+
+      const reader = new FileReader()
+
+      reader.onload = e => {
+        const newAvatar = e.target?.result as string
+
+        console.log(newAvatar)
+        // if (newAvatar) {
+        //   onAvatarChange(newAvatar)
+        // }
+      }
+      reader.readAsDataURL(file)
+    }
+  }
+
   return (
     <Modal onOpenChange={onOpenChange} open={open} title={'Add New Card'}>
-      Lorem ipsum dolor sit amet
+      Question:
+      <TextField fullWidth label={'Question?'} />
+      <>
+        <Button className={s.editAvatarButton} onClick={selectFileHandler}>
+          <Edit2 />
+        </Button>
+        <input
+          accept={'image/*'}
+          onChange={uploadHandler}
+          ref={inputRef}
+          style={{ display: 'none' }}
+          type={'file'}
+        />
+      </>
+      <Modal.Footer
+        countButton={2}
+        firstButtonName={'Add New Card'}
+        secondButtonHandler={onOpenChange}
+        secondButtonName={'Cancel'}
+      />
     </Modal>
   )
 }
