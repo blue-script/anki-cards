74fc419ef3f7a1ac6412e78898c0c6074b6b6a8e
apelsinom <apelsinom@mail.ru>
2024-05-28 14:07:24 +0300

    19 feat: fix-2 Decks19

--- src/pages/decks19/decks19.tsx
+++ src/pages/decks19/decks19.tsx
@@ -1,43 +1,49 @@
 import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 import { DecksTable } from '@/entities'
 import { useGetDecksQuery } from '@/services/decks/decks.service'
-import { Button, Page, Pagination, Slider, TabSwitcher, TextField, Typography } from '@/shared'
+import { Button, FormTextField, Page, Pagination, Slider, TabSwitcher, Typography } from '@/shared'
 import { useDebounce } from '@/shared/hooks/useDebounce'
 
 import s from './decks19.module.scss'
 
 import SvgTrashOutline from './../../assets/icons/TrashOutline'
 
+const tabsSwitcher = [
+  { disabled: false, text: 'My Cards', value: 'My Cards' },
+  { disabled: false, text: 'All Cards', value: 'All Cards' },
+]
+
 export const Decks19 = () => {
   const [search, setSearch] = useState('')
-  const [itemsPerPage, setItemsPerPage] = useState(10)
+  const [tabSwitcherValue, setTabSwitcherValue] = useState('All Cards')
+  const [itemsPerPage, setItemsPerPage] = useState(5)
+  const [currentPage, setCurrentPage] = useState(1)
   const debouncedSearch = useDebounce(search, 1500)
-  const {
-    data: decks,
-    error,
-    isLoading,
-  } = useGetDecksQuery({
+  const { data: decks } = useGetDecksQuery({
+    currentPage: currentPage,
     itemsPerPage: itemsPerPage,
     name: debouncedSearch,
   })
-
-  const tabs = [
-    { disabled: false, text: 'My Cards', value: 'My Cards' },
-    { disabled: false, text: 'All Cards', value: 'All Cards' },
-  ]
-
-  const pagination = {
-    currentPage: 1,
-    pageSize: 1,
-    siblingCount: 1,
-    totalCount: 4,
-  }
-  const handleSearch = (search: string) => {
+  const { control, reset } = useForm<{ name: string }>({
+    defaultValues: { name: '' },
+  })
+  const handleSearchChange = (search: string) => {
     setSearch(search)
   }
   const handleItemsPerPage = (page: number) => {
     setItemsPerPage(page)
+  }
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page)
+  }
+  const resetFilters = () => {
+    reset()
+    setSearch('')
+  }
+  const handleValueChange = (value: string) => {
+    setTabSwitcherValue(value)
   }
 
   return (
@@ -49,29 +55,39 @@ export const Decks19 = () => {
         </Button>
       </div>
       <div className={s.filter}>
-        <TextField onValueChange={handleSearch} value={search} variant={'search'} />
+        <FormTextField
+          control={control}
+          name={'name'}
+          onValueChange={handleSearchChange}
+          placeholder={'Input search'}
+          variant={'search'}
+        />
         <TabSwitcher
           label={'Show decks cards'}
-          onValueChange={value => value}
-          tabs={tabs}
-          value={'All Cards'}
+          onValueChange={handleValueChange}
+          tabs={tabsSwitcher}
+          value={tabSwitcherValue}
         />
         <Slider label={'Number of cards'} />
-        <Button variant={'secondary'}>
+        <Button onClick={resetFilters} variant={'secondary'}>
           <SvgTrashOutline />
           Clear Filter
         </Button>
       </div>
       <div className={s.table}>
-        <DecksTable decks={decks?.items} onDeleteClick={id => id} onEditClick={id => id} />
+        <DecksTable
+          currentUserId={decks?.}
+          decks={decks?.items}
+          onDeleteClick={id => id}
+          onEditClick={id => id}
+        />
       </div>
       <Pagination
-        currentPage={pagination.currentPage}
-        onPageChange={(value: number) => value}
-        pageSize={pagination.pageSize}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+        pageSize={itemsPerPage}
         setPageSize={handleItemsPerPage}
-        siblingCount={pagination.siblingCount}
-        totalCount={pagination.totalCount}
+        totalCount={decks?.pagination.totalItems || 50}
       />
     </Page>
   )
