29bb4771f1246c4745c7cde38fbed1d2a2e8f55c
Aleksandr Demianov <92304847+blue-script@users.noreply.github.com>
2024-05-30 22:11:52 +0500

    feat: add image to card (#89)
    * feat: add image to card
    
    * feat: add image to card and types

--- src/entities/deck/addCardModal/addCardModal.tsx
+++ src/entities/deck/addCardModal/addCardModal.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import toast from 'react-hot-toast'
 import { useParams } from 'react-router-dom'
@@ -15,11 +14,13 @@ import s from './addCardModal.module.scss'
 
 const addCardSchema = z.object({
   answer: z.string(),
-  answerImg: z.string().optional(),
+  answerImg: z.instanceof(File).nullable().optional(),
   id: z.string(),
   question: z.string(),
-  questionImg: z.string().optional(),
+  questionImg: z.instanceof(File).nullable().optional(),
 })
+
+type CreateCardSchema = z.infer<typeof addCardSchema>
 
 type Props = {
   onOpenChange: () => void
@@ -30,29 +31,26 @@ export const AddCardModal = ({ onOpenChange, open }: Props) => {
   const { deckId } = useParams<{ deckId: string }>()
   const [createCard] = useCreateCardMutation()
 
-  const [questionImg, setQuestionImg] = useState<string>('')
-  const [answerImg, setAnswerImg] = useState<string>('')
-
-  const { control, handleSubmit, reset } = useForm<CreateCardArgs>({
-    defaultValues: { answer: '', answerImg: '', id: deckId, question: '', questionImg: '' },
+  const { control, handleSubmit, reset, setValue, watch } = useForm<CreateCardSchema>({
+    defaultValues: { answer: '', answerImg: null, id: deckId, question: '', questionImg: null },
     resolver: zodResolver(addCardSchema),
   })
+
+  const questionImg = watch('questionImg')
+  const answerImg = watch('answerImg')
 
   const submitHandler = handleSubmit((data: CreateCardArgs) => {
     const formData = new FormData()
 
-    // data.questionImg = questionImg
-    // data.answerImg = answerImg
-
     formData.append('answer', data.answer)
     formData.append('question', data.question)
-    if (questionImg) {
-      formData.append('questionImg', questionImg)
+    if (data.questionImg) {
+      formData.append('questionImg', data.questionImg)
+    }
+    if (data.answerImg) {
+      formData.append('answerImg', data.answerImg)
     }
 
-    if (answerImg) {
-      formData.append('answerImg', answerImg)
-    }
     createCard({ data: formData, id: deckId })
       .unwrap()
       .then(() => {
@@ -70,9 +68,16 @@ export const AddCardModal = ({ onOpenChange, open }: Props) => {
           <div>
             Question:
             <FormTextField control={control} fullWidth label={'Question?'} name={'question'} />
-            {questionImg && <img alt={'Uploaded'} className={s.img} src={questionImg} />}
+            {questionImg && (
+              <img alt={'Uploaded'} className={s.img} src={URL.createObjectURL(questionImg)} />
+            )}
           </div>
-          <ImageUpload handleChangeImage={setQuestionImg} variantButton={'secondary'}>
+          <ImageUpload
+            control={control}
+            name={'questionImg'}
+            setValue={setValue}
+            variantButton={'secondary'}
+          >
             <Layer2 />
             <Typography option={'subtitle2'}>
               {questionImg ? 'Change Image' : 'Upload Image'}
@@ -81,9 +86,16 @@ export const AddCardModal = ({ onOpenChange, open }: Props) => {
           <div>
             Answer:
             <FormTextField control={control} fullWidth label={'Answer'} name={'answer'} />
-            {answerImg && <img alt={'Uploaded'} className={s.img} src={answerImg} />}
+            {answerImg && (
+              <img alt={'Uploaded'} className={s.img} src={URL.createObjectURL(answerImg)} />
+            )}
           </div>
-          <ImageUpload handleChangeImage={setAnswerImg} variantButton={'secondary'}>
+          <ImageUpload
+            control={control}
+            name={'answerImg'}
+            setValue={setValue}
+            variantButton={'secondary'}
+          >
             <Layer2 />
             <Typography option={'subtitle2'}>
               {answerImg ? 'Change Image' : 'Upload Image'}
