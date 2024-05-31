999f5a5fbf58d5283870969921dd2a894cfa98a5
Aleksandr Demianov <92304847+blue-script@users.noreply.github.com>
2024-05-31 17:28:55 +0500

    Edit and learn cards (#91)
    * feat: edit name deck
    
    * feat: function learn card
    
    * feat: function delete deck

--- src/assets/icons/MoreVerticalOutline.tsx
+++ src/assets/icons/MoreVerticalOutline.tsx
@@ -3,23 +3,37 @@ import { Ref, SVGProps, forwardRef, memo } from 'react'
 const SvgMoreVerticalOutline = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
   <svg
     fill={'none'}
-    height={24}
+    height={'18'}
     ref={ref}
-    width={24}
+    viewBox={'0 0 18 18'}
+    width={'18'}
     xmlns={'http://www.w3.org/2000/svg'}
     {...props}
   >
-    <circle cx={12} cy={12} r={8.5} stroke={'#fff'} />
-    <g clipPath={'url(#more-vertical-outline_svg__a)'} fill={'#fff'}>
+    <circle cx={'9'} cy={'9'} r={'8.5'} stroke={'white'} />
+    <g clipPath={'url(#clip0_78129_3534)'}>
       <path
         d={
-          'M12 13a1 1 0 1 0 0-2 1 1 0 0 0 0 2M12 9.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2M12 16.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2'
+          'M9 10C9.55228 10 10 9.55228 10 9C10 8.44772 9.55228 8 9 8C8.44772 8 8 8.44772 8 9C8 9.55228 8.44772 10 9 10Z'
         }
+        fill={'white'}
+      />
+      <path
+        d={
+          'M9 6.5C9.55228 6.5 10 6.05228 10 5.5C10 4.94772 9.55228 4.5 9 4.5C8.44772 4.5 8 4.94772 8 5.5C8 6.05228 8.44772 6.5 9 6.5Z'
+        }
+        fill={'white'}
+      />
+      <path
+        d={
+          'M9 13.5C9.55228 13.5 10 13.0523 10 12.5C10 11.9477 9.55228 11.5 9 11.5C8.44772 11.5 8 11.9477 8 12.5C8 13.0523 8.44772 13.5 9 13.5Z'
+        }
+        fill={'white'}
       />
     </g>
     <defs>
-      <clipPath id={'more-vertical-outline_svg__a'}>
-        <path d={'M6 6h12v12H6z'} fill={'#fff'} />
+      <clipPath id={'clip0_78129_3534'}>
+        <rect fill={'white'} height={'12'} transform={'translate(3 3)'} width={'12'} />
       </clipPath>
     </defs>
   </svg>
