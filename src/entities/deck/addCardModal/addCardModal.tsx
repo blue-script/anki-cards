88823c28fce4729d7f821e9cce6f03d3d2a564f0
blue-script <alexm92@inbox.ru>
2024-06-06 20:19:52 +0500

    feat: formModal for add card

--- src/entities/deck/addCardModal/addCardModal.tsx
+++ src/entities/deck/addCardModal/addCardModal.tsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import toast from 'react-hot-toast'
 import { useParams } from 'react-router-dom'
 
 import { Layer2 } from '@/assets/icons'
+import { FormData } from '@/entities/deck/addCardModal/formData/formData'
 import { useCreateCardMutation } from '@/services/cards/cards.service'
 import { CardArgs } from '@/services/cards/cards.types'
 import { FormTextField, ImageUpload, Modal, Typography } from '@/shared'
@@ -14,13 +16,13 @@ import s from './addCardModal.module.scss'
 
 const addCardSchema = z.object({
   answer: z.string(),
-  answerImg: z.instanceof(File).nullable().optional(),
+  answerImg: z.union([z.instanceof(File), z.string()]).nullable(),
   id: z.string(),
   question: z.string(),
-  questionImg: z.instanceof(File).nullable().optional(),
+  questionImg: z.union([z.instanceof(File), z.string()]).nullable(),
 })
 
-type CreateCardSchema = z.infer<typeof addCardSchema>
+export type CreateCardSchema = z.infer<typeof addCardSchema>
 
 type Props = {
   onOpenChange: () => void
@@ -29,70 +31,80 @@ type Props = {
 
 export const AddCardModal = ({ onOpenChange, open }: Props) => {
   const { deckId } = useParams<{ deckId: string }>()
+
+  const [questionImgPreview, setQuestionImgPreview] = useState<null | string>('')
+  const [answerImgPreview, setAnswerImgPreview] = useState<null | string>('')
+
   const [createCard] = useCreateCardMutation()
 
   const { control, handleSubmit, reset, setValue, watch } = useForm<CreateCardSchema>({
-    defaultValues: { answer: '', answerImg: null, id: deckId, question: '', questionImg: null },
+    defaultValues: {
+      answer: '',
+      answerImg: null,
+      id: deckId,
+      question: '',
+      questionImg: null,
+    },
     resolver: zodResolver(addCardSchema),
   })
 
   const questionImgWatch = watch('questionImg')
   const answerImgWatch = watch('answerImg')
 
+  useEffect(() => {
+    // questionImgPreview && URL.revokeObjectURL(questionImgPreview)
+    if (questionImgWatch instanceof File) {
+      setQuestionImgPreview(URL.createObjectURL(questionImgWatch))
+    }
+
+    return () => {
+      questionImgPreview && URL.revokeObjectURL(questionImgPreview)
+    }
+  }, [questionImgWatch, questionImgPreview])
+
+  useEffect(() => {
+    if (answerImgWatch instanceof File) {
+      setAnswerImgPreview(URL.createObjectURL(answerImgWatch))
+    }
+
+    return () => {
+      answerImgPreview && URL.revokeObjectURL(answerImgPreview)
+    }
+  }, [answerImgWatch, answerImgPreview])
+
   if (!deckId) {
     return <div>Error</div>
   }
 
-  const submitHandler = handleSubmit((data: CardArgs) => {
-    const formData = new FormData()
-
-    formData.append('answer', data.answer)
-    formData.append('question', data.question)
-    if (data.questionImg) {
-      formData.append('questionImg', data.questionImg)
+  const submitHandler = handleSubmit(async (data: CardArgs) => {
+    try {
+      await createCard({ data, deckId })
+      reset()
+      toast.success('Card added successfully!')
+    } catch (e) {
+      toast.error('Failed to add card')
+    } finally {
+      onOpenChange()
     }
-    if (data.answerImg) {
-      formData.append('answerImg', data.answerImg)
-    }
-
-    createCard({ data: formData, deckId })
-      .unwrap()
-      .then(() => {
-        reset()
-        toast.success('Card added successfully!')
-      })
-      .catch(() => toast.error('Failed to add card'))
-      .finally(() => onOpenChange())
   })
 
   return (
     <Modal onOpenChange={onOpenChange} open={open} title={'Add New Card'}>
       <form onSubmit={submitHandler}>
         <div className={s.body}>
-          <div>
-            Question:
-            <FormTextField control={control} fullWidth label={'Question?'} name={'question'} />
-            {questionImgWatch && (
-              <img alt={'Uploaded'} className={s.img} src={URL.createObjectURL(questionImgWatch)} />
-            )}
-          </div>
-          <ImageUpload
+          <FormData
             control={control}
-            name={'questionImg'}
+            imgName={'questionImg'}
+            imgPreview={questionImgPreview}
+            imgWatch={questionImgWatch}
+            placeholder={'Question'}
             setValue={setValue}
-            variantButton={'secondary'}
-          >
-            <Layer2 />
-            <Typography option={'subtitle2'}>
-              {questionImgWatch ? 'Change Image' : 'Upload Image'}
-            </Typography>
-          </ImageUpload>
+            textName={'question'}
+          />
           <div>
             Answer:
             <FormTextField control={control} fullWidth label={'Answer'} name={'answer'} />
-            {answerImgWatch && (
-              <img alt={'Uploaded'} className={s.img} src={URL.createObjectURL(answerImgWatch)} />
-            )}
+            {answerImgPreview && <img alt={'Uploaded'} className={s.img} src={answerImgPreview} />}
           </div>
           <ImageUpload
             control={control}
---
+++ src/entities/deck/addCardModal/formData/formData.tsx
@@ -0,0 +1,44 @@
+import { Control, UseFormSetValue } from 'react-hook-form'
+
+import { Layer2 } from '@/assets/icons'
+import { CreateCardSchema } from '@/entities/deck/addCardModal/addCardModal'
+import { FormTextField, ImageUpload, Typography } from '@/shared'
+
+import s from './formData.module.scss'
+
+type Props = {
+  control: Control<CreateCardSchema>
+  imgName: 'answerImg' | 'questionImg'
+  imgPreview: null | string
+  imgWatch: File | null | string
+  placeholder: string
+  setValue: UseFormSetValue<CreateCardSchema>
+  textName: 'answer' | 'question'
+}
+
+export const FormData = ({
+  control,
+  imgName,
+  imgPreview,
+  imgWatch,
+  placeholder,
+  setValue,
+  textName,
+}: Props) => {
+  return (
+    <div className={s.container}>
+      {placeholder}:
+      <FormTextField control={control} fullWidth label={`${placeholder}?`} name={`${textName}`} />
+      {imgWatch && imgPreview && <img alt={'Uploaded'} className={s.img} src={imgPreview} />}
+      <ImageUpload
+        control={control}
+        name={`${imgName}`}
+        setValue={setValue}
+        variantButton={'secondary'}
+      >
+        <Layer2 />
+        <Typography option={'subtitle2'}>{imgWatch ? 'Change Image' : 'Upload Image'}</Typography>
+      </ImageUpload>
+    </div>
+  )
+}
