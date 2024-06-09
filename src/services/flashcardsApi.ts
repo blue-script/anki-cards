ee8356456e8f0593d51847847072e185e178fbc6
Aleksandr Demianov <92304847+blue-script@users.noreply.github.com>
2024-06-09 22:30:01 +0500

    Update flashcardsApi.ts for storybook

--- src/services/flashcardsApi.ts
+++ src/services/flashcardsApi.ts
@@ -2,25 +2,25 @@ import { baseQueryWithReauth } from '@/services/flashCardsBaseQuery'
 import { createApi } from '@reduxjs/toolkit/query/react'
 
 export const flashcardsApi = createApi({
-  baseQuery: baseQueryWithReauth,
-  // baseQuery: fetchBaseQuery({
-  //   baseUrl: 'https://api.flashcards.andrii.es',
-  //   prepareHeaders: headers => {
-  //     const token = localStorage.getItem('accessToken')
-  //
-  //     if (headers.get('Authorization')) {
-  //       return headers
-  //     }
-  //
-  //     if (token) {
-  //       headers.set('Authorization', `Bearer ${token}`)
-  //     }
-  //   },
-  //   // credentials: 'include',
-  //   // prepareHeaders: headers => {
-  //   //   headers.append('x-auth-skip', 'true')
-  //   // },
-  // }),
+  // baseQuery: baseQueryWithReauth,
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.flashcards.andrii.es',
+    prepareHeaders: headers => {
+      const token = localStorage.getItem('accessToken')
+  
+      if (headers.get('Authorization')) {
+        return headers
+      }
+  
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`)
+      }
+    },
+    // credentials: 'include',
+    // prepareHeaders: headers => {
+    //   headers.append('x-auth-skip', 'true')
+    // },
+  }),
   endpoints: () => ({}),
   keepUnusedDataFor: 1,
   reducerPath: 'flashcardsApi',
